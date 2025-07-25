10ad80e2c8723dece000332272b4df1e4671e05d
Sheik Hassan <sheik_hassan@comcast.com>
2025-07-24 23:22:03 -0400

    Fixed mobile issues

--- components/project.tsx
+++ components/project.tsx
@@ -8,10 +8,6 @@ import { motion, useScroll, useTransform } from 'framer-motion';
 // Shortcut to props
 type ProjectProps = (typeof projectsData)[number];
 
-// Project Card Component
-// Can use section or article tag
-// article tag, take it out of this website and put other place
-// self contained thing.
 export default function Project({
   title,
   description,
@@ -36,8 +32,8 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last-mb-0"
     >
-      <section className=" rounded-lg bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 flex flex-col relative sm:h-[20rem] sm:mb-8 last:mb-0 group-even:pl-8 hover:bg-gray-200 transition">
-        <div className="py-4 px-5 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+      <section className="rounded-lg bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 flex flex-col relative sm:h-[20rem] sm:mb-8 last:mb-0 group-even:pl-8 hover:bg-gray-200 transition">
+        <div className="py-4 px-5 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
           <ul className="flex flex-wrap gap-2 mt-4 mb-6 sm:mt-auto">
@@ -51,18 +47,25 @@ export default function Project({
             ))}
           </ul>
         </div>
+
+        {/* Mobile image (only shows on small screens) */}
+        <div className="relative h-60 sm:hidden w-full mt-4">
+          <Image
+            src={imageUrl}
+            alt={`${title} project`}
+            fill
+            className="object-cover rounded-lg"
+            sizes="(max-width: 640px) 80vw"
+            quality={95}
+          />
+        </div>
+
+        {/* Desktop image (preserves all existing animations) */}
         <Image
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute w-[20rem] sm:w-[28.5rem] top-8 -right-50 sm:-right-40 rounded-t-lg shadow-2xl
-            group-even:right-[initial] group-even:-left-50
-            group-hover:-translate-x-3
-            group-hover:translate-y-3
-            group-hover:-rotate-2
-            group-even:group-hover:translate-x-3
-            group-even:group-hover:-translate-y-3
-            group-even:group-hover:rotate-2
+          className="hidden sm:flex absolute w-[20rem] sm:w-[28.5rem] top-8 -right-50 sm:-right-40 rounded-t-lg shadow-2xl
             sm:group-even:right-[initial] sm:group-even:-left-40
             sm:group-hover:-translate-x-3
             sm:group-hover:translate-y-3
